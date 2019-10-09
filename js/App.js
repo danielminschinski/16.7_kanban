42fdde77cecac690bad71c61face74a1a481536c
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-09 21:03:09 +0200

    Update script colimn.js

--- js/App.js
+++ js/App.js
@@ -1,10 +1,11 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 4006
-  'X-Auth-Token': addc60e4d2e475e6c8521e555e8e4394
+  'X-Client-Id': '4006',
+  'X-Auth-Token': 'addc60e4d2e475e6c8521e555e8e4394'
 };
 
-fetch(baseUrl + '/board', { headers: myHeaders })
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
 	.then(function(resp){
 		return resp.json();
 	})
